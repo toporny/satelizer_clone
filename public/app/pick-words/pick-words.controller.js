d191d4c043fa140f435174856aaa694f2e837156
Przemyslaw Rzeznik <przemyslaw.rzeznik@3vcorporate.com>
2016-11-04 15:27:19 +0000

    select languages working

--- public/app/pick-words/pick-words.controller.js
+++ public/app/pick-words/pick-words.controller.js
@@ -6,6 +6,30 @@
     PickCtrl.$inject = [];
 
     function PickCtrl ($scope, $http) {
+		vm = this;
+		vm.choose = choose;
+
+		var languages = [
+			{ value: ''     , label: '--select--'  },
+			{ value: 'FR_fr', label: 'French'  },
+			{ value: 'GE_ge', label: 'German' },
+			{ value: 'SP_sp', label: 'Spain' },
+			{ value: 'RU_ru', label: 'Russian' },
+			{ value: 'PL_pl', label: 'Polish' }
+		];
+	
+		vm.data = {
+			languages: languages,
+			languageSelected: languages[0], // we need this to pick first element as selected
+		};
+
+
+		vm.data.selected = vm.data.languages[0];
+
+		function choose( data) {
+			console.log(data);
+		}
+
 
     }
 
