ca92dc6515ae0e5fad6a61772a853949390401c4
toporny <toporny@tlen.pl>
2016-11-06 21:51:55 +0000

    add directive to make dynamic buttons

--- public/app/pick-words/pick-words.controller.js
+++ public/app/pick-words/pick-words.controller.js
@@ -1,4 +1,29 @@
 (function () {
+angular
+	.module('MyApp')
+	.directive('showLevelButtons', function() {
+		return {
+			restrict: 'E',
+			scope: {
+				selcd: '@',
+				free: '@'
+			},
+			link: function (scope, element, attrs) {
+				console.log(attrs);
+				scope.aha = '333..'+ attrs.selcd;
+			},
+			template: '<div style="border:1px solid red">{{aha}} + {{selcd}} + {{free}}</div>'
+		};
+		function showLevelButtonsCtrl() {
+
+		}
+	});
+})();
+
+
+
+
+(function () {
   angular
     .module('MyApp')
     .controller('PickCtrl',PickCtrl);
@@ -9,8 +34,20 @@
 		vm = this;
 		vm.choose = choose;
 
+
+		// todo: move it to config files
+		var config = {
+			'FR_fr': { label: 'French', words:4000, levels:4, free_levels: 2  },
+			'GE_ge': { label: 'German', words:3000, levels:5, free_levels: 3},
+			'SP_sp': { label: 'Spain',  words:10000, levels:5, free_levels: 3},
+			'RU_ru': { label: 'Russian', words:10000, levels:5, free_levels: 3},
+			'PL_pl': { label: 'Polish', words:5000, levels:5, free_levels: 2},
+			'EN_en': { label: 'English', words:4000, levels:4, free_levels: 2}
+		};
+	
+
 		var languages = [
-			{ value: ''     , label: '--select--'  },
+			{ value: '-'     , label: '--select--'  },
 			{ value: 'FR_fr', label: 'French'  },
 			{ value: 'GE_ge', label: 'German' },
 			{ value: 'SP_sp', label: 'Spain' },
@@ -20,6 +57,7 @@
 	
 		vm.data = {
 			languages: languages,
+			config: config, 
 			languageSelected: languages[0], // we need this to pick first element as selected
 		};
 
