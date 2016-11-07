7c94d8a80625d7121367cb42263745bb92bff195
Przemyslaw Rzeznik <przemyslaw.rzeznik@3vcorporate.com>
2016-11-07 15:46:46 +0000

    learn improvement..

--- public/app/pick-words/pick-words.controller.js
+++ public/app/pick-words/pick-words.controller.js
@@ -3,16 +3,22 @@ angular
 	.module('MyApp')
 	.directive('showLevelButtons', function() {
 		return {
-			restrict: 'E',
+			restrict: 'A',
 			scope: {
-				selcd: '@',
-				free: '@'
+				showLevelButtons: '@'
 			},
 			link: function (scope, element, attrs) {
-				console.log(attrs);
-				scope.aha = '333..'+ attrs.selcd;
+				// scope.$watch('showLevelButtons', function(newValue, oldValue) {
+				// 	if (newValue) console.log("I see a data change!");
+				// });
+
+				// console.log(attrs);
+				// scope.aha = attrs.showLevelButtons ;
+				// console.log('aha',scope.aha);
+
 			},
-			template: '<div style="border:1px solid red">{{aha}} + {{selcd}} + {{free}}</div>'
+			template: '<div style="border:1px solid red">{{showLevelButtons.value}}</div>'+ 
+			'<div ng-repeat="n in [].constructor(4) track by $index"> {{$index}} </div>'
 		};
 		function showLevelButtonsCtrl() {
 
@@ -37,17 +43,17 @@ angular
 
 		// todo: move it to config files
 		var config = {
-			'FR_fr': { label: 'French', words:4000, levels:4, free_levels: 2  },
-			'GE_ge': { label: 'German', words:3000, levels:5, free_levels: 3},
-			'SP_sp': { label: 'Spain',  words:10000, levels:5, free_levels: 3},
-			'RU_ru': { label: 'Russian', words:10000, levels:5, free_levels: 3},
-			'PL_pl': { label: 'Polish', words:5000, levels:5, free_levels: 2},
-			'EN_en': { label: 'English', words:4000, levels:4, free_levels: 2}
+			'FR_fr': { label: 'French',  words:4000,  levels:3, free_levels: 1 },
+			'GE_ge': { label: 'German',  words:3000,  levels:3, free_levels: 2 },
+			'SP_sp': { label: 'Spain',   words:10000, levels:4, free_levels: 3 },
+			'RU_ru': { label: 'Russian', words:10000, levels:5, free_levels: 3 },
+			'PL_pl': { label: 'Polish',  words:5000,  levels:3, free_levels: 2 },
+			'EN_en': { label: 'English', words:4000,  levels:4, free_levels: 2 }
 		};
 	
 
 		var languages = [
-			{ value: '-'     , label: '--select--'  },
+			{ value: ''     , label: '--select--'  },
 			{ value: 'FR_fr', label: 'French'  },
 			{ value: 'GE_ge', label: 'German' },
 			{ value: 'SP_sp', label: 'Spain' },
