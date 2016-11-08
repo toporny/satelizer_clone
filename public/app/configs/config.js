a0951d3545ed60602ba48464977e4379c7b18ac7
toporny <toporny@tlen.pl>
2016-11-08 17:02:14 +0000

    remove vendor static files and add bower.json with vendor definitions

---
+++ public/app/configs/config.js
@@ -0,0 +1,18 @@
+(function() {
+  angular
+    .module('MyApp')
+    .constant('apiUrl', 'http://localhost:3000/')
+
+    .constant('definedLanguages',{
+        'FR_fr': { label: 'French',  words:4000,  free_words: 2000 },
+        'GE_ge': { label: 'German',  words:3000,  free_words: 2000 },
+        'SP_sp': { label: 'Spain',   words:10000, free_words: 2000 },
+        'RU_ru': { label: 'Russian', words:10000, free_words: 2000 },
+        'PL_pl': { label: 'Polish',  words:5000,  free_words: 2000 },
+        'EN_en': { label: 'English', words:4000,  free_words: 2000 }
+    });
+    
+
+
+})();
+
--- public/app/pick-words/pick-words.controller.js
+++ public/app/pick-words/pick-words.controller.js
@@ -1,35 +1,4 @@
 (function () {
-angular
-	.module('MyApp')
-	.directive('showLevelButtons', function() {
-		return {
-			restrict: 'A',
-			scope: {
-				showLevelButtons: '@'
-			},
-			link: function (scope, element, attrs) {
-				// scope.$watch('showLevelButtons', function(newValue, oldValue) {
-				// 	if (newValue) console.log("I see a data change!");
-				// });
-
-				// console.log(attrs);
-				// scope.aha = attrs.showLevelButtons ;
-				// console.log('aha',scope.aha);
-
-			},
-			template: '<div style="border:1px solid red">{{showLevelButtons.value}}</div>'+ 
-			'<div ng-repeat="n in [].constructor(4) track by $index"> {{$index}} </div>'
-		};
-		function showLevelButtonsCtrl() {
-
-		}
-	});
-})();
-
-
-
-
-(function () {
   angular
     .module('MyApp')
     .controller('PickCtrl',PickCtrl);
@@ -38,20 +7,10 @@ angular
 
     function PickCtrl ($scope, $http) {
 		vm = this;
-		vm.choose = choose;
+		vm.next = next;
+		vm.showModal = showModal;
 
-
-		// todo: move it to config files
-		var config = {
-			'FR_fr': { label: 'French',  words:4000,  levels:3, free_levels: 1 },
-			'GE_ge': { label: 'German',  words:3000,  levels:3, free_levels: 2 },
-			'SP_sp': { label: 'Spain',   words:10000, levels:4, free_levels: 3 },
-			'RU_ru': { label: 'Russian', words:10000, levels:5, free_levels: 3 },
-			'PL_pl': { label: 'Polish',  words:5000,  levels:3, free_levels: 2 },
-			'EN_en': { label: 'English', words:4000,  levels:4, free_levels: 2 }
-		};
-	
-
+		// get this from DB
 		var languages = [
 			{ value: ''     , label: '--select--'  },
 			{ value: 'FR_fr', label: 'French'  },
@@ -63,16 +22,23 @@ angular
 	
 		vm.data = {
 			languages: languages,
-			config: config, 
+			//config: config, 
 			languageSelected: languages[0], // we need this to pick first element as selected
 		};
 
 
 		vm.data.selected = vm.data.languages[0];
 
-		function choose( data) {
-			console.log(data);
+		function next() {
+			console.log('startLevel!, languageSelected = ',vm.data.languageSelected);
+			alert('next = ');
+
 		}
+
+		function showModal() {
+			console.log('showModal');
+			alert('showModal ');
+		}		
 
 
     }
