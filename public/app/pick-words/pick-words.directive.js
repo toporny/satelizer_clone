1f9653e18461b6527707075a5fbb586e0b8050f2
toporny <toporny@tlen.pl>
2016-11-13 00:55:45 +0000

    new look on PICK WORDS

--- public/app/pick-words/pick-words.directive.js
+++ public/app/pick-words/pick-words.directive.js
@@ -1,56 +1,56 @@
-(function () {
-
-angular
-	.module('MyApp')
-	.directive('showLevelButtons', function(definedLanguages) {
-		return {
-			restrict: 'A',
-			scope: {
-				languageSelected: '=languageSele',
-				showModal: '&',
-				next: '&' 
-			},
-
-			link: link,
-
-			template: [
-				'<div ng-repeat="n in aWords track by $index">',
-				'	<span ng-if="n.free != 1" >',
-				'	  <button ng-click="next()" type="button" class="btn btn-success" style="margin:4px; width:110px">',
-				'	  <i class="ion-play"></i>',
-				'	Level {{ n.level }}</button>({{ n.from }}-{{ n.to }} most used words)',
-				'	</span>',
-				'	<span ng-if="n.free == 1" >',
-				'	  <button ng-click="showModal()" type="button" class="btn btn-danger" style="margin:4px; width:110px">',
-				'	  <i class="ion-locked"></i>',
-				'	Level {{ n.level }}</button>({{ n.from }}-{{ n.to }} most used words)',
-				'	</span>',
-				'</div>'
-			].join('')
-		};
-
-		function link (scope, element, attrs) {
-
-			scope.$watch('languageSelected', function(newValue, oldValue) {
-				//if (newValue) console.log("I see a data change!", newValue.value);
-				//console.log( definedLanguages[newValue.value].label);
-				if (angular.isDefined(newValue.value) && newValue.value ) {
-
-					var freeWords = definedLanguages[newValue.value].free_words;
-					var words = definedLanguages[newValue.value].words;
-
-					scope.aWords = [];
-					var free=0;
-					for (i=0; i<words/500; i++) {
-						if ((i*500) >= freeWords) free = 1; else free = 0;
-						scope.aWords.push({level: i+1, from:500*i, to: 500*i+500, free:free});
-					}
-					//scope.languageSelected2 = definedLanguages[newValue.value].free_levels;
-				} 
-				else scope.aWords = [];
-			});
-		}
-
-
-	});
-})();
+// (function () {
+// 
+// angular
+	// .module('MyApp')
+	// .directive('showLevelButtons', function(definedLanguages) {
+		// return {
+			// restrict: 'A',
+			// scope: {
+				// languageSelected: '=languageSele',
+				// showModal: '&',
+				// next: '&' 
+			// },
+// 
+			// link: link,
+// 
+			// template: [
+				// '<div ng-repeat="n in aWords track by $index">',
+				// '	<span ng-if="n.free != 1" >',
+				// '	  <button ng-click="next()" type="button" class="btn btn-success" style="margin:4px; width:110px">',
+				// '	  <i class="ion-play"></i>',
+				// '	Level {{ n.level }}</button>({{ n.from }}-{{ n.to }} most used words)',
+				// '	</span>',
+				// '	<span ng-if="n.free == 1" >',
+				// '	  <button ng-click="showModal()" type="button" class="btn btn-danger" style="margin:4px; width:110px">',
+				// '	  <i class="ion-locked"></i>',
+				// '	Level {{ n.level }}</button>({{ n.from }}-{{ n.to }} most used words)',
+				// '	</span>',
+				// '</div>'
+			// ].join('')
+		// };
+// 
+		// function link (scope, element, attrs) {
+// 
+			// scope.$watch('languageSelected', function(newValue, oldValue) {
+				// if (newValue) console.log("I see a data change!", newValue.value);
+				// console.log( definedLanguages[newValue.value].label);
+				// if (angular.isDefined(newValue.value) && newValue.value ) {
+// 
+					// var freeWords = definedLanguages[newValue.value].free_words;
+					// var words = definedLanguages[newValue.value].words;
+// 
+					// scope.aWords = [];
+					// var free=0;
+					// for (i=0; i<words/500; i++) {
+						// if ((i*500) >= freeWords) free = 1; else free = 0;
+						// scope.aWords.push({level: i+1, from:500*i, to: 500*i+500, free:free});
+					// }
+//					scope.languageSelected2 = definedLanguages[newValue.value].free_levels;
+				// } 
+				// else scope.aWords = [];
+			// });
+		// }
+// 
+// 
+	// });
+// })();
