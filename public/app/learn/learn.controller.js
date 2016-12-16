c5ba709f647765ee831fa1eeeba8d5729536338e
toporny <toporny@tlen.pl>
2016-12-16 23:00:31 +0000

    nice scroll animation with transitions

--- public/app/learn/learn.controller.js
+++ public/app/learn/learn.controller.js
@@ -3,17 +3,37 @@
     .module('MyApp')
     .controller('LearnCtrl',LearnCtrl);
 
-    LearnCtrl.$inject = ['$translate', 'maxWordsPerPage', 'translatePluginToISO', 'user', 'availableDictionaries'];
+    LearnCtrl.$inject = ['$scope', '$translate', 'maxWordsPerPage', 'translatePluginToISO', 'user', 'availableDictionaries'];
 
 //				http://vt-api.com.es/vocabulary-trainer.php
 
 
-    function LearnCtrl ($translate, maxWordsPerPage, translatePluginToISO, user, availableDictionaries ) {
+    function LearnCtrl ($scope, $translate, maxWordsPerPage, translatePluginToISO, user, availableDictionaries ) {
     	vm = this;
+    	vm.add_at_bottom = add_at_bottom;
+    	vm.add_at_top = add_at_top;
+    	vm.remove_at_bottom = remove_at_bottom;
+    	vm.remove_at_top = remove_at_top;
+    	vm.scroll_up = scroll_up;
+    	vm.scroll_down = scroll_down;
+
 			vm.data = {
 				//availableDictionaries : {},
 				languageToLearn  : 'en_EN',
-				maxWordsPerPage: maxWordsPerPage
+				maxWordsPerPage: maxWordsPerPage,
+				myValue: false,
+				showFirst: true,
+				myValue2: 30,
+				slowo : 'word3',
+				items: [
+					{id:1, word:'word1'},
+					{id:2, word:'word2'},
+					{id:3, word:'word3'},
+					{id:4, word:'word4'},
+					{id:5, word:'word5'},
+					{id:5, word:'word6'},
+					{id:5, word:'word7'},
+				]
 //ngStorage-languageToLearn
 				// languages: languages,
 				// //config: config, 
@@ -24,6 +44,18 @@
           {
             languageToLearn: 'en_EN'
           });
+
+
+          $scope.$watchCollection(
+              "learn.data.items",
+              function( newValue, oldValue ) {
+                  console.log('oldValue',oldValue);
+                  console.log('newValue',newValue);
+
+              }
+          );
+
+
 
 			//showLeftButtons();
 
@@ -46,7 +78,41 @@
 			// }
 
 
+			function add_at_top(){
+				vm.data.items.unshift({id:6, hide: 'false', word:'word6'});
+			}
+
+			function add_at_bottom(){
+				vm.data.items.push({id:6, hide: 'false', word:'word6'});
+			}
+			
+			function remove_at_top(){
+				vm.data.items.splice(0,1);
+			}
+
+			function remove_at_bottom(){
+				vm.data.items.pop();
+			}
+
+ 
+			function scroll_up(){
+				vm.data.slowo = 'word2'; 
+				add_at_top();
+				remove_at_bottom();
+			}
+
+			function scroll_down(){
+				vm.data.slowo = 'word4'; 
+				remove_at_top();
+				add_at_bottom();
+			}
+
+ 
+
+
     }
+
+
 
 })();
 
